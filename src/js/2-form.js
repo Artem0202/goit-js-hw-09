53930c131931e7764516603d190ebe5a1b2c74f1
Artem0202 <artyomkov.19@gmail.com>
2024-01-26 00:40:32 +0200

    Update 2-form.js

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,13 +1,29 @@
 'use strict';
 const feedbackForm = document.querySelector('.feedback-form');
-const textarea = feedbackForm.elements.email;
+const messageInput = feedbackForm.elements.message;
+const emailInput = feedbackForm.elements.email;
 const localStorageKey = 'feedback-form-state';
+const savedForm = {
+  email: emailInput.value,
+  message: messageInput.value,
+};
+const savedFormData = JSON.parse(localStorage.getItem(localStorageKey)) || {};
 
-textarea.value = localStorage.getItem(localStorageKey) ?? '';
+emailInput.value = savedFormData.email ?? '';
+messageInput.value = savedFormData.message ?? '';
 
-feedbackForm.addEventListener('input', event => {
-  localStorage.setItem(localStorageKey, JSON.stringify(event.target.value));
-});
+function localStorageForm(event) {
+  if (event.target == emailInput) {
+    savedForm.email = event.target.value.trim();
+    savedForm.message = messageInput.value;
+  }
+  if (event.target == messageInput) {
+    savedForm.message = event.target.value.trim();
+    savedForm.email = emailInput.value;
+  }
+
+  localStorage.setItem(localStorageKey, JSON.stringify(savedForm));
+}
 
 function formHandler(event) {
   event.preventDefault();
@@ -27,4 +43,5 @@ function formHandler(event) {
   form.reset();
 }
 
+feedbackForm.addEventListener('input', localStorageForm);
 feedbackForm.addEventListener('submit', formHandler);
